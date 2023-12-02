c6dfc2d64e06828996ea129c3890561d18beb848
Uğur <133202238+ugurcomptech@users.noreply.github.com>
2023-12-02 23:57:32 +0300

    Update access_log.php

--- access_log.php
+++ access_log.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Access Log</title>
-    <!-- Bootstrap CSS bağlantısı -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
@@ -47,30 +46,8 @@
     </table>
 </div>
 
-<!-- Bootstrap JS ve jQuery bağlantıları -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
